0b4ffe3dcc9dd544c31b510f27289e5e1fb85f2d
Sreejith Sreekantan <imjith@gmail.com>
2019-06-29 12:33:05 +0530

    Enable EHCache

--- src/main/java/in/sjstudio/hibernate/advanced/JpaWithHibernateAdvancedApplication.java
+++ src/main/java/in/sjstudio/hibernate/advanced/JpaWithHibernateAdvancedApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.math.BigDecimal;
 
+
 @SpringBootApplication
 public class JpaWithHibernateAdvancedApplication implements CommandLineRunner {
 
--- src/main/java/in/sjstudio/hibernate/advanced/entity/Course.java
+++ src/main/java/in/sjstudio/hibernate/advanced/entity/Course.java
@@ -2,15 +2,10 @@ package in.sjstudio.hibernate.advanced.entity;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 @Entity
+@Cacheable
 public class Course {
 
   @Id
